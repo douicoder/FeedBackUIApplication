17827b71d41a59619ba49eef9d3d123129c96a2b
Doui GURVINDER <doui.gurvinder@outlook.com>
2024-12-19 05:26:43 -0800

    Add project files.

---
+++ FeedBackUIApplication/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FeedBackUIApplication
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FeedBackUIApplication/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using SignInAndSignUpUI;
+
+namespace FeedBackUIApplication
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private  void TapGestureRecognizer_Tapped_For_SignUP(object sender, EventArgs e)
+        {
+            count++;
+        }
+    }
+
+}
